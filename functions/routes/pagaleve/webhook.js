5c91e69ab1abe65fc2b4c0747e6c8d754045d78e
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2024-04-06 23:16:42 -0300

    chore(webhook): get status order from pagaleve before update order in ecom

--- functions/routes/pagaleve/webhook.js
+++ functions/routes/pagaleve/webhook.js
@@ -25,12 +25,16 @@ const parseStatusToEcom = (pagaleveTransactionStatus) => {
       return 'paid'
 
     case 'expired':
-    case 'DECLINED':
-    case 'ABANDONED':
+    case 'declined':
+    case 'abandoned':
     case 'canceled':
       return 'voided'
   }
   return 'unknown' // INTERNAL_ERROR
+}
+
+const validateStatus = function (status) {
+  return status >= 200 && status <= 301
 }
 
 exports.post = ({ appSdk, admin }, req, res) => {
@@ -41,81 +45,76 @@ exports.post = ({ appSdk, admin }, req, res) => {
   const {
     id,
     orderReference,
-    state,
     amount
   } = body
   console.log('>> Store: ', storeId, ' body: ', JSON.stringify(body), ' <<')
   if (storeId > 100) {
-    res.status(200).send(body)
     return appSdk.getAuth(storeId)
       .then(async (auth) => {
         try {
-          const order = await findOrderById(appSdk, storeId, auth, orderReference)
-          if (order) {
-            // update payment
-            const transactionId = order.transactions[0]._id
-            let body = {
-              date_time: new Date().toISOString(),
-              status: parseStatusToEcom(state),
-              transaction_id: transactionId,
-              flags: ['Pagaleve']
-            }
-            const responsePaymentHistory = await appSdk.apiRequest(
-              storeId,
-              `orders/${order._id}/payments_history.json`,
-              'POST',
-              body,
-              auth
-            )
-            if (responsePaymentHistory) {
-              console.log('> Transaction Code Pagaleve <')
-              body = {
-                intermediator: {
-                  transaction_id: id || '',
-                  transaction_code: id || ''
+          const appData = await getAppData({ appSdk, storeId, auth })
+          const pagaleveAxios = new PagaLeveAxios(appData.username, appData.password, false, storeId)
+          await pagaleveAxios.preparing
+          const { axios } = pagaleveAxios
+          return axios.get(`/v1/purchase-requests/${id}`, {
+            maxRedirects: 0,
+            validateStatus
+          }).then(({ data }) => {
+            console.log('>> Get payment status <<', JSON.stringify(data))
+            const state = data.status
+            const order = await findOrderById(appSdk, storeId, auth, orderReference)
+            if (order) {
+              const transaction = order.transactions.find(({ intermediator }) => {
+                return intermediator && intermediator.transaction_id === id
+              })
+              if (transaction && transaction._id) {
+                // update payment
+                const transactionId = transaction._id
+                const responsePaymentHistory = await appSdk.apiRequest(
+                  storeId,
+                  `orders/${order._id}/payments_history.json`,
+                  'POST',
+                  {
+                    date_time: new Date().toISOString(),
+                    status: parseStatusToEcom(state),
+                    transaction_id: transactionId,
+                    flags: ['Pagaleve']
+                  },
+                  auth
+                )
+                if (responsePaymentHistory) {
+                  console.log('updated order', order._id)
                 }
-              }
-            }
-            const responseUpdateTransaction = await appSdk.apiRequest(
-              storeId,
-              `orders/${order._id}/transactions/${transactionId}.json`,
-              'PATCH',
-              body,
-              auth
-            )
-            if (responseUpdateTransaction) {
-              console.log('> UPDATE Transaction OK')
-            }
-            if (state.toLowerCase() === 'authorized') {
-              getAppData({ appSdk, storeId, auth })
-                .then(appData => {
-                  const pagaleveAxios = new PagaLeveAxios(appData.username, appData.password, false, storeId)
-                  pagaleveAxios.preparing
-                    .then(() => {
-                      const { axios } = pagaleveAxios
-                      let body = {
-                        checkout_id: id,
-                        currency: 'BRL',
-                        amount,
-                        intent: 'CAPTURE'
-                      }
-                      console.log('> SendPayment Pagaleve: ', JSON.stringify(body), ' <<')
-                      // https://axios-http.com/ptbr/docs/req_config
-                      const validateStatus = function (status) {
-                        return status >= 200 && status <= 301
-                      }
-                      return axios.post('/v1/payments', body, { 
-                        maxRedirects: 0,
-                        validateStatus
-                      })
-                    })
-                    .then(({ data }) => {
-                      console.log('>> Created payment <<', JSON.stringify(data))
-                    })
-                    .catch(error => {console.log('erro', error)})
+                if (state && state.toLowerCase() === 'paid') {
+                  const pagalevePayment = {
+                    checkout_id: id,
+                    currency: 'BRL',
+                    amount,
+                    intent: 'CAPTURE'
+                  }
+                  console.log('> SendPayment Pagaleve: ', JSON.stringify(pagalevePayment), ' <<')
+                  // https://axios-http.com/ptbr/docs/req_config
+                  
+                  return axios.post('/v1/payments', {
+                    body:pagalevePayment
+                  }, { 
+                    maxRedirects: 0,
+                    validateStatus
+                  }).then(({ data }) => {
+                    console.log('>> Created payment <<', JSON.stringify(data))
+                    res.status(200).send(body)
+                  })
+                  .catch(error => {console.log('erro na criação do pagamento', error)})
+                }
+              } else {
+                console.log('não encontrou pedido')
+                res.status(400).send({
+                  error: 'não encontrou o pedido'
                 })
+              } 
             }
-          }
+          })
+          .catch(error => {console.log('erro pra buscar status', error); throw error})
         } catch (error) {
           console.error(error)
           const { response, config } = error
